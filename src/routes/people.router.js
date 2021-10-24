ea25eb207d1a86edbe866f4e8ca3e80b3301f43a
DrisHel <doris90nunes@gmail.com>
2021-10-24 14:10:21 -0300

    ajustado o prefix do module. exports.

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -1,6 +1,6 @@
 const PeopleController = require('../app/controller/PeopleController');
 
-module.exports = (server, routes, prefix = '/people') => {
+module.exports = (server, routes, prefix = '/api/v1/people/') => {
     routes.post('/',PeopleController.create);
     server.use(prefix, routes);
   }
