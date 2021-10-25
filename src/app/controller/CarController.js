11f838e4aedc95aaebad1bc3c1d61b7371fd27e7
DrisHel <doris90nunes@gmail.com>
2021-10-24 23:38:42 -0300

    Removido comentarios do controller.

--- src/app/controller/CarController.js
+++ src/app/controller/CarController.js
@@ -6,8 +6,6 @@ class CarController  {
     const result = await CarService.create(req.body);
     return res.status(201).json(result)
   }
-
-
   async getAll(req,res){
     const result = await CarService.listAll(req.query)
     return res.status(200).json(result);
@@ -16,56 +14,14 @@ class CarController  {
     const result = await CarService.deleteCar(req.params.id)
     return res.status(204).json(result);
   }
-
   async update(req,res){
     const result = await CarService.updateCar(req.params.id, req.body)
     return res.status(200).json(result);
   }
-
   async getById(req,res){
     const result = await CarService.getByIdCar(req.params.id)
     return res.status(200).json(result);
   }
-
-
-
-
 }
-
-
- /* async getByModelo(req,res){
-    const {modelo}=req.query;
-    const car = await Car.find({modelo});
-    
-    return res.status(200).json(car);
-  }
-
-  async getByCor(req,res){
-    const {cor}=req.query;
-    const car = await Car.find({cor});
-    
-    return res.status(200).json(car);
-  }
-
-  async getByAno(req,res){
-    const {ano}=req.query;
-    const car = await Car.find({ano});
-    
-    return res.status(200).json(car);
-  }
-
-  async getByAcessorio(req,res){
-    const {acessorio}=req.query;
-    const car = await Car.find({acessorio});
-    
-    return res.status(200).json(car);
-  }
-  async getByQuantidadePassajeiro(req,res){
-    const {quantidadePassajeiro}=req.query;
-    const car = await Car.find({quantidadePassajeiro});
-    
-    return res.status(200).json(car);
-  }
-}*/
 
 module.exports = new CarController();
